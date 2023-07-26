6f8e0e3252bc6f155825d1a05f11c9105b1d39d3
Spicoud <113991629+sara-picoud@users.noreply.github.com>
2023-07-26 14:42:49 +0200

    Update PanelContent.js
    add codeMirror for editor live

--- src/components/PanelContent.js
+++ src/components/PanelContent.js
@@ -1,21 +1,40 @@
 import React from "react";
-
-import SyntaxHighlighter from "./SyntaxHighlighter";
-import style from "react-syntax-highlighter/dist/esm/styles/hljs/github";
+import CodeMirror from "@uiw/react-codemirror";
 
 /**
  * Checkout https://github.com/storybookjs/storybook/blob/next/code/addons/jest/src/components/Panel.tsx
  * for a real world example
  */
-export const PanelContent = ({ code, showLineNumbers, wrapLines }) => (
-  <SyntaxHighlighter
-    language={"xml"}
-    copyable={true}
-    padded={true}
-    style={style}
-    showLineNumbers={showLineNumbers}
-    wrapLines={wrapLines}
-  >
-    {code}
-  </SyntaxHighlighter>
-);
+export const PanelContent = ({ code}) => {
+  const [codeState, setCodeState] = React.useState('')
+
+  React.useEffect(() => {
+    setCodeState(code || '')
+  }, [code])
+
+  const onChange = React.useCallback((value) => {
+    setCodeState(value);
+  }, []);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      const storybookPreviewWrapper = document.getElementById('storybook-preview-iframe');
+      if (storybookPreviewWrapper && storybookPreviewWrapper.contentDocument) {
+        const storybookRoot = storybookPreviewWrapper.contentDocument.getElementById('storybook-root').firstChild;
+        if (storybookRoot) {
+          storybookRoot.innerHTML = codeState;
+        }
+      }
+    }, 10);
+  }, [codeState]);
+
+    return (
+      <CodeMirror
+        basicSetup={{lineNumbers: true, completionKeymap: true, highlightActiveLineGutter: true, autocompletion: true}}
+        value={codeState}
+        height="100%"
+        onChange={onChange}
+      />
+    )
+
+}
